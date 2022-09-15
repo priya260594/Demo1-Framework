faafc7ec4408c12878b08a5318ae1f026ae85642
HP <HP@192.168.0.102>
2022-09-15 19:36:04 +0530

    first commit by engineer 1-addProductToCart

---
+++ src/test/java/Practice/addToCartTest.java
@@ -0,0 +1,13 @@
+package Practice;
+
+import org.testng.annotations.Test;
+
+public class addToCartTest {
+
+@Test
+public void addProductToCart() {
+	System.out.println("product is added");
+}
+
+
+}
